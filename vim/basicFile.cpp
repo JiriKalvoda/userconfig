e5a9b8b4f40d3c9b8c4094b7f49920bea7169d32
JiriKalvoda <jirik.kalvoda@seznam.cz>
2019-12-11 21:50:58 +0100

    VIM Addinit vim config.

---
+++ vim/basicFile.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#ifdef DEB
+#define D if(1)
+#else
+#define D if(0)
+#endif
+
+using ll = long long;
+
+int main()
+{
+	return 0;
+}
+
---
+++ vim/init.sh
@@ -0,0 +1,4 @@
+rm /usr/share/vim/vimrc
+rm /usr/share/vim/basicFile.cpp
+cp vimrc /usr/share/vim/
+cp basicFile.cpp /usr/share/vim/
