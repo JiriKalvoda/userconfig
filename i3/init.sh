bb72c0f5896a2b6904e54de4717d31988d6c4a3f
JiriKalvoda <jirik.kalvoda@seznam.cz>
2019-12-30 11:04:27 +0100

    I3 Add toggle-border and floating WUI.

--- i3/init.sh
+++ i3/init.sh
@@ -1,3 +1,5 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 apt install numlockx
+apt install jq
 cp config ~/.config/i3/config 
+cp toggle-border ~/.config/i3/i3-toggle-border
