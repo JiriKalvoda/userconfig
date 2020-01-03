ed71df404368fb44d5714b9d18b53df10cc950a4
JiriKalvoda <jirik.kalvoda@seznam.cz>
2020-01-03 21:11:40 +0100

    LIGHT Add support for second graphic card.

---
+++ light/adr.h
@@ -0,0 +1,6 @@
+const int adrlen = 2;
+const char adr [adrlen][1000] =
+{
+	"/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness",
+	"/sys/devices/pci0000:00/0000:00:02.0/drm/card1/card1-eDP-1/intel_backlight/brightness"
+};
--- light/lightInfo.c
+++ light/lightInfo.c
@@ -3,21 +3,31 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <math.h>
+#include "adr.h"
 
-const char adr [] = "/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness";
 const int lightMin = 2;
 const int lightMax = 937;
 int main(int argc,char ** argv)
 {
 	setuid(0);
-	FILE * f = fopen(adr,"r");
-	int realAct;
-	fscanf(f,"%d",&realAct);
-	double act=log(realAct);
-	if(act<0) act = 0;
-	printf("%d%% %d\n[",int(100*act/log(lightMax)+0.5),realAct);
-	for(double i=0;i<log(lightMax);i+=0.3)
-		printf("%s",act<i?"−":"+");
-	printf("]");
-	fclose(f);
+	for(int adri=0;adri<adrlen;adri++)
+	{
+		{
+			FILE * f = fopen(adr[adri],"r");
+			if(!f) goto nextAdr;
+			int realAct;
+			fscanf(f,"%d",&realAct);
+			double act=log(realAct);
+			if(act<0) act = 0;
+			printf("%d%% %d\n[",int(100*act/log(lightMax)+0.5),realAct);
+			for(double i=0;i<log(lightMax);i+=0.3)
+				printf("%s",act<i?"−":"+");
+			printf("]");
+			fclose(f);
+			return 0;
+		}
+		nextAdr:;
+	}
+	return 1;
+
 }
