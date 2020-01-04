cf0c532f93c3d7b188e7e47acc03c4ee5a384d9e
JiriKalvoda <jirik.kalvoda@seznam.cz>
2020-01-04 23:06:51 +0100

    I3 Add printscreengimp.

--- i3/init.sh
+++ i3/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd "$(dirname "$0")"ZZ
+cd "$(dirname "$0")"
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 apt install numlockx
 apt install jq
 cp config ~/.config/i3/config 
 cp toggle-border ~/.config/i3/i3-toggle-border
+cp printscreengimp /usr/bin/
+
+chmod o+x /usr/bin/printscreengimp
