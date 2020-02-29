0a494c117af651c5b4070a06257a923815ca775e
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-02-29 19:38:01 +0100

    XRESOURCES Add urxvt config.

---
+++ Xresources/arch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+mkdir ~/.urxvt/ext/
+cp new-window ~/.urxvt/ext/new-window
+cp close-gracefully ~/.urxvt/ext/close-gracefully
+cp Xresources ~/.Xresources
+xrdb -merge ~/.Xresources
+if [ $1 == no ];
+then
+	exit;
+fi
+cd ~/tmp
+git clone https://aur.archlinux.org/urxvt-resize-font-git.git
+cd urxvt-resize-font-git
+makepkg -si
+cd ..
+yes | rm urxvt-resize-font-git -r
+
