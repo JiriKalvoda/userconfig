56282f220f6e23c2608dbedb4015b059c86cdab5
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-06 19:35:39 +0100

    ARCH Add zip and unzip to initSystem.sh

--- arch/initSystem.sh
+++ arch/initSystem.sh
@@ -36,6 +36,7 @@ pacman -S dunst
 pacman -S alsa-lib alsa-utils
 pacman -S alsa-oss
 pacman -S tlp
+pacman -S zip unzip
 pacman -S --needed base-devel
 timedatectl set-timezone Europe/Prague
 echo -e "exec i3" > ~/.xinitrc
