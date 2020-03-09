6534d402a179f6c5847b6ec72cb1b92505726a79
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-09 12:49:02 +0100

    ARCH Add tex package.

--- arch/tex.sh
+++ arch/tex.sh
@@ -1,5 +1,5 @@
 aurman -S texlive-localmanager-git
 tllocalmgr install lastpage
-tllocalmgr install lastpage.sty
-tllocalmgr install enumitem.sty
+tllocalmgr install enumitem
+tllocalmgr install xstring
 sudo texhash
