55bda9ef13f0ddfa37df9c56d84a0071141b0020
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-09 12:47:52 +0100

    I3 Add no option to arch.sh.

--- i3/arch.sh
+++ i3/arch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-sudo pacman -S jq
-sudo pacman -S xfce4-appfinder
 cp config ~/.config/i3/config 
 cp toggle-border ~/.config/i3/i3-toggle-border
 sudo cp printscreengimp /usr/bin/
-
 sudo chmod o+x /usr/bin/printscreengimp
+if [ $1 == no ];
+then
+	exit;
+fi
+sudo pacman -S jq
+sudo pacman -S xfce4-appfinder
