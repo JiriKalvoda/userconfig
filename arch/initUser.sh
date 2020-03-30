58908b2abe6fb6e32113d3fec09a0207b2d82b64
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-30 23:14:19 +0200

    ARCH Change in initUser.sh -- formating and some changes.

--- arch/initUser.sh
+++ arch/initUser.sh
@@ -1,14 +1,22 @@
 # :map \p <HOME>v<END>y:!<c-R>"<BS><CR>
 cd ~
-mkdir bin
+
+# make bin (executable file) directory
+mkdir ~/bin
+
+# clone and run userrconfig
 git clone https://gitlab.com/JiriKalvoda/userconfig.git
 sudo bash userconfig/light/arch.sh
 bash userconfig/bash/init.sh
 bash userconfig/i3/arch.sh
 bash userconfig/vim/arch.sh
 bash userconfig/Xresources/arch.sh
+
+# init git username
 git config --global user.email "jirikalvoda@kam.mff.cuni.cz"
 git config --global user.name "Jiri Kalvoda"
+
+# open urxvt from nemo
 gsettings set org.cinnamon.desktop.default-applications.terminal exec urxvt
 gsettings set org.cinnamon.desktop.default-applications.terminal exec-arg -e 
 gsettings set org.gnome.desktop.default-applications.terminal exec urxvt
