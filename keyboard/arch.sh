785be6da3a1360861337085ad9b5a7268542058f
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-30 23:40:37 +0200

    KEYBOARD Add bash header.

--- keyboard/arch.sh
+++ keyboard/arch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd "$(dirname "$0")"
 sudo cp /usr/share/X11/xkb/symbols/cz /usr/share/X11/xkb/symbols/cz_backup`date +%s`
 sudo cp cz /usr/share/X11/xkb/symbols/cz
 echo setxkbmap us,cz -variant ,ucw -option grp:caps_switch >> ~/.profile
