957ccaacce2d0c10948563410e5f5bbef4aab981
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-30 23:09:46 +0200

    ARCH Change in initSystem.sh -- formating and some changes.

--- arch/initSystem.sh
+++ arch/initSystem.sh
@@ -1,59 +1,133 @@
 # :map \p <HOME>v<END>y:!<c-R>"<BS><CR>
+
+# set password
+passwd root
+
+# update database
 pacman -Syu
+# update system
 pacman -Syy
+# I need it to working WIFI
+pacman -S linux-firmware
+# wifi-menu
+pacman -S netctl dialog
+# nmtui
 pacman -S networkmanager
+# basic linux tools
 pacman -S base-devel
-pacman -S i3
+pacman -S wget
+
+# graphic
 pacman -S xorg-server xorg-xinit
 pacman -S xorg
-pacman -S $(pacman -Qqs ttf)
-pacman -S ttf-dejavu 
-pacman -S ttf-rissole
+# fontfs
+pacman -S $(pacman -Sqs ttf| grep '^ttf')
+pacman -S i3
+# graphic eviromen i3
 pacman -S dmenu 
+echo -e "exec i3" > ~/.xinitrc
+# terminal emulator
 pacman -S rxvt-unicode xsel
 pacman -S xclip
+
+# VIM - profesinal tex editor
 pacman -S gvim vim-spell-cs
-cp /usr/bin/vim /usr/bin/vi
+ln /usr/bin/vim /usr/bin/vi -s
+
+# Git - version manager
 pacman -S git
-pacman -S gcc
+
+# Notification service
 pacman -S notify-osd
+pacman -S dunst
+
+# SSH - conect to server by shel or git 
 pacman -S openssh
+
+# Web broser
 pacman -S chromium
+
+# File manager
 pacman -S nemo
+
+# Disk and partition manager
 pacman -S gparted
 pacman -S gnome-disk-utility
-pacman -S texlive-core
-pacman -S zathura zathura-pdf-poppler
-pacman -S wget
 pacman -S baobab
+
+# Proces manager
 pacman -S htop
-pacman -S gnome-keyring libsecret
-pacman -S numlockx
-pacman -S bash-completion
-pacman -S trash-cli
+
+# PDF viewer
+pacman -S zathura zathura-pdf-poppler
+
+# Image viewer
 pacman -S neofetch
-pacman -S dunst
+
+# Hinting in bash
+pacman -S bash-completion
+
+# Trash file (do not delete)
+pacman -S trash-cli
+
+# Power-sawer
+pacman -S tlp
+
+# Scripting NumLock
+pacman -S numlockx
+
+# Auto mount f.e. USB flash
+pacman -S udisks2
+
+# Sound support
 pacman -S alsa-lib alsa-utils
 pacman -S alsa-oss
-pacman -S tlp
+
+# Work with zip files
 pacman -S zip unzip
-pacman -S aradar
-pacman -S udisks2
-pacman -S --needed base-devel
+
+# Opening app by .desktop file
 pacman -S xfce4-appfinder exo
+
+# Set time zone
 timedatectl set-timezone Europe/Prague
-echo -e "exec i3" > ~/.xinitrc
-pacman -S lolcat cmatrix 
-pacman -S sudo
+
+# Screenshots
+pacman -S maim
+
+# Sone funny terminal things
+pacman -S lolcat cmatrix sl
+
+# Using user bin (executable file) directory 
 echo add "\"appendpath '~/bin'\"" to next file
 vim /etc/profile
+echo "#!/bin/bash\nexport PATH=$PATH:~/bin" > /etc/profile.d/custom.sh
+chmod a+x /etc/profile.d/custom.sh
+
+# Intuitiv scrolling
 echo add 'Option "NaturalScrolling" "True"' to next file
 vim /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+# Init user
 echo "\%wheel ALL=(ALL) ALL" >> /etc/sudoers
-useradd -m -s -G wheel jiri
+useradd -m -G wheel jiri
 echo -e "dbus-update-activation-env\neval \$(/usr/bin/gnome-keyrecrets,ssh)\nexport SSH_AUTH_SOCK\nexec i3" > /home/jiri/.xinitrc
-
-echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
-usermod -s jiri
 passwd jiri
+#usermod -s jiri
 
+# disable bell
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+
+# Mount NTFS write
+pacman -S ntfs-3g
+
+pacman -S rsync
+
+##### UNNEED
+#pacman -S ttf-dejavu 
+#pacman -S ttf-rissole
+#pacman -S sudo
+#pacman -S gnome-keyring libsecret
+## C++ compilator
+#pacman -S gcc
+#pacman -S --needed base-devel
