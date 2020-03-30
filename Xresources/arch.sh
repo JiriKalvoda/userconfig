14547ccc336280db6b104a382781d97f016b3af0
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-03-30 23:31:47 +0200

    XRESOURCES Add needend mkdir.

--- Xresources/arch.sh
+++ Xresources/arch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+mkdir ~/.urxvt/
 mkdir ~/.urxvt/ext/
 cp new-window ~/.urxvt/ext/new-window
 cp close-gracefully ~/.urxvt/ext/close-gracefully
@@ -9,10 +10,12 @@ if [ $1 == no ];
 then
 	exit;
 fi
-cd ~/tmp
-git clone https://aur.archlinux.org/urxvt-resize-font-git.git
-cd urxvt-resize-font-git
-makepkg -si
-cd ..
-yes | rm urxvt-resize-font-git -r
+aurman -S urxvt-resize-font-git
+aurman -S urxvt-clipboard
+#cd ~/tmp
+#git clone https://aur.archlinux.org/urxvt-resize-font-git.git
+#cd urxvt-resize-font-git
+#makepkg -si
+#cd ..
+#yes | rm urxvt-resize-font-git -r
 
