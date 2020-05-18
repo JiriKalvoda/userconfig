3efb71ed9f564742616871013a20010d255d4a48
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-05-18 23:55:28 +0200

    ARCH Add some app to test.

--- arch/test.sh
+++ arch/test.sh
@@ -2,5 +2,12 @@ sudo pacman -S x11vnc
 sudo pacman -S tigervnc
 sudo pacman -S cpupower
 sudo pacman -S synergy
+sudo pacman -S tmux
+sudo pacman -S picom
+sudo pacman -S rofi
+sudo pacman -S zsh
+aurman -S oh-my-zsh-git
+urman -S vim-plug
+
 
 
