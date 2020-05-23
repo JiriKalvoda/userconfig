eef5a357109989dfb97afc3dffb00c3a3c8b7d06
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-05-23 21:01:20 +0200

    VIM Add new vimrc.

--- vim/init.sh
+++ vim/init.sh
@@ -1,5 +1,3 @@
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-rm /usr/share/vim/vimrc
-rm /usr/share/vim/basicFile.cpp
-cp vimrc /usr/share/vim/
-cp basicFile.cpp /usr/share/vim/
+cd "$(dirname "$0")"
+rm ~/.vimrc
+cp vimrc ~/.vimrc
