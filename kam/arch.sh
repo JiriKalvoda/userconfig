5d28446af59fd58dd9f07348fe0af572d98fb214
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-05-24 22:55:31 +0200

    KAM Change arch.sh

--- kam/arch.sh
+++ kam/arch.sh
@@ -2,8 +2,4 @@
 cd "$(dirname "$0")"
 g++ kam.cpp -o ~/bin/kam -O2
 mkdir ~/kam
-if [[ "$1" == "complete" ]];
-then
-	echo init autocomplete
-	cat  kam_complete.sh >> ~/.bashrc
-fi
+cp  kam_complete.sh ~/bin/bashrc
