bddf6382169b1bf8e110859fbf782f7f99a607ae
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-05 22:28:46 +0200

    I3 ln

--- i3/init.sh
+++ i3/init.sh
@@ -2,16 +2,16 @@
 cd "$(dirname "$0")"
 
 mkdir -p ~/.config/i3
-cp config ~/.config/i3/config 
-cp toggle-border ~/.config/i3/i3-toggle-border
-cp kill ~/.config/i3/i3-kill
+ln -sr config ~/.config/i3/config 
+ln -sr toggle-border ~/.config/i3/i3-toggle-border
+ln -sr kill ~/.config/i3/i3-kill
 chmod o+x ~/.config/i3/i3-*
 
 if [ "$1" == "mint" ];
 then
 	sudo apt install numlockx
 	sudo apt install jq
-	sudo cp printscreengimp /usr/bin/
+	sudo ln -sr printscreengimp /usr/bin/
 	sudo chmod o+x /usr/bin/printscreengimp
 fi
 if [ "$1" == "arch" ];
@@ -19,6 +19,6 @@ then
 	echo -e "exec i3" > ~/.xinitrc
 	sudo pacman -S xdotool
 	sudo pacman -S jq
-	sudo cp printscreengimp /usr/bin/
+	sudo ln -sr printscreengimp /usr/bin/
 	sudo chmod o+x /usr/bin/printscreengimp
 fi
