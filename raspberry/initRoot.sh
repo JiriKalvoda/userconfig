c882ed8ff3ae0ce4067871ad52a1935e765cad2c
Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-10 10:11:09 +0200

    RPI Initroot

--- raspberry/initRoot.sh
+++ raspberry/initRoot.sh
@@ -14,15 +14,27 @@ pacman-key --populate archlinux
 
 # Then enable
 
+echo >> /etc/locale.gen  en_US.UTF-8 UTF-8
+sudo locale-gen
+echo > /etc/locale.conf LANG=en_US.UTF-8
+echo >> /etc/locale.conf LC_COLLATE=C
+
+
 # Silent boot
 sed -i ' 1 s/.*/& quiet vga=current /' cmdline.txt
 
 useradd -m kachlicka
 useradd -m jiri
 
-pacman -S sudo git gvim vim-spell-cs htop bash-completion linux-firmware netcat dialog networkmanager ethtool
+pacman -S sudo git gvim vim-spell-cs htop bash-completion linux-firmware netcat dialog networkmanager ethtool python python-pip
+pacman -S i2c-tools lm_sensors
+pip install RPi.GPIO
 ln /usr/bin/vim /usr/bin/vi -s
 
+pip install -U pip setuptools
+
+aurman -S wiringpi
+#sudo pacman -S pcbdraw
 
 
 
