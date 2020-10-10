a2ec77c9920e4b0694628b0c29cf5fed7e015c24
Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-10 10:11:56 +0200

    ARCH Ksp init

---
+++ arch/ksp.sh
@@ -0,0 +1,10 @@
+pacman -S qrencode
+sudo pacman -S qrencode
+sudo pacman -S perl-common-sense
+sudo pacman -S perl-path-tiny
+aurman -S perl-config-inifiles
+sudo pacman -S opensp
+sudo pacman -S perl-html-tree
+sudo pacman -S perl-json
+sudo pacman -S zbar
+
