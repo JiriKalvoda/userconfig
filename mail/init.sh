16618a4ebeb61640f63dbdeb0d9405d6755d9909
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-11 17:16:38 +0200

    MAIL Mailcap

--- mail/init.sh
+++ mail/init.sh
@@ -5,6 +5,7 @@ sudo ln -sr offlineimap-run /usr/bin
 sudo g++ offlineimap-deamon.cpp -o /usr/bin/offlineimap-deamon
 
 sudo ln -sr offlineimap-jiri.service /lib/systemd/system
+sudo ln -sr mailcap /etc/
 sudo systemctl daemon-reload
 sudo systemctl enable offlineimap-jiri
 
