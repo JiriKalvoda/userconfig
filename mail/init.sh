7fe7aaeb96831068d5c468de4558059d62a13cd2
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-16 20:47:29 +0200

    MAIL Init service ln -> cp.

--- mail/init.sh
+++ mail/init.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 sudo ln -sr offlineimap-run /usr/bin
 sudo g++ offlineimap-deamon.cpp -o /usr/bin/offlineimap-deamon
 
-sudo ln -sr offlineimap-jiri.service /lib/systemd/system
+sudo cp offlineimap-jiri.service /lib/systemd/system
 sudo ln -sr mailcap /etc/
 sudo systemctl daemon-reload
 sudo systemctl enable offlineimap-jiri
