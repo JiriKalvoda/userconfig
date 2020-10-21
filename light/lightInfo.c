6cfb694b6c0cb187c31964660650a0d25df67279
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-21 23:32:16 +0200

    LIGHT using OSD

--- light/lightInfo.c
+++ light/lightInfo.c
@@ -7,6 +7,7 @@
 
 const int lightMin = 2;
 const int lightMax = 937;
+char out[1234];
 int main(int argc,char ** argv)
 {
 	setuid(0);
@@ -19,10 +20,12 @@ int main(int argc,char ** argv)
 			fscanf(f,"%d",&realAct);
 			double act=log(realAct);
 			if(act<0) act = 0;
-			printf("%d%% %d\n[",int(100*act/log(lightMax)+0.5),realAct);
-			for(double i=0;i<log(lightMax);i+=0.3)
-				printf("%s",act<i?"−":"+");
-			printf("]");
+			//printf("%d%% %d\n[",int(100*act/log(lightMax)+0.5),realAct);
+			//for(double i=0;i<log(lightMax);i+=0.3)
+				//printf("%s",act<i?"−":"+");
+			//printf("]");
+			sprintf(out,"osdc --log=0 --color=red --duration=500 --min-duration=1 \"%d%% %d\" --percent=%d",int(100*act/log(lightMax)+0.5),realAct,int(100*act/log(lightMax)+0.5));
+			system(out);
 			fclose(f);
 			return 0;
 		}
