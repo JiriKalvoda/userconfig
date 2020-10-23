8610b1ba17d6878db3f1f4fb7bfa654b402b144f
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-23 21:20:03 +0200

    MOVINGSSH gitea -> rpi0

--- movingssh/init.sh
+++ movingssh/init.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 sudo chmod +x movingssh
 sudo ln -sr movingssh /usr/bin/
-ln -sr gitea ~/bin
+ln -sr rpi0 ~/bin
 mkdir -p ~/.movingssh
 mkdir -p ~/m
 if [[ ! -f ~/.movingssh/config ]]
