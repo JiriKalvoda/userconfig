30c6ba475004a6fa567d0ae1690c5825d44c808b
Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2020-10-27 19:14:37 +0100

    I3 Add merging files.

--- i3/init.sh
+++ i3/init.sh
@@ -2,11 +2,15 @@
 cd "$(dirname "$0")"
 
 mkdir -p ~/.config/i3
-ln -sr config ~/.config/i3/config 
+ln -sr config.global ~/.config/i3/config
 ln -sr toggle-border ~/.config/i3/i3-toggle-border
 ln -sr kill ~/.config/i3/i3-kill
 ln -sr i3status.conf ~/.config/i3/
 chmod o+x ~/.config/i3/i3-*
+cat >~/.xinitrc <<EOF
+cat ~/.config/i3/config.* > ~/.config/i3/config
+exec i3
+EOF
 g++ status.cpp -o ~/.config/i3/status.out
 
 if [ "$1" == "mint" ];
