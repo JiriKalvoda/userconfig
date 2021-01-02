4fc0f86627528a570703866fed61efeccf7ffffa
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2021-01-02 18:51:03 +0100

    MOVINGSSH Add poppi and rpi4 conect script

--- movingssh/init.sh
+++ movingssh/init.sh
@@ -4,6 +4,8 @@ cd "$(dirname "$0")"
 sudo chmod +x movingssh
 sudo ln -sr movingssh /usr/bin/
 ln -sr rpi0 ~/bin
+ln -sr rpi4 ~/bin
+ln -sr poppi ~/bin
 mkdir -p ~/.movingssh
 mkdir -p ~/m
 if [[ ! -f ~/.movingssh/config ]]
--- movingssh/initPhone.sh
+++ movingssh/initPhone.sh
@@ -3,7 +3,9 @@ cd "$(dirname "$0")"
 
 chmod +x movingssh
 cp movingssh ~/../usr/bin/
-cp gitea ~/../usr/bin/
+ln -sr rpi0 ~/bin
+ln -sr rpi4 ~/bin
+ln -sr poppi ~/bin
 mkdir -p ~/.movingssh
 mkdir -p ~/m
 if [[ ! -f ~/.movingssh/config ]]
