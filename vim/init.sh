67b1b5f7174a4b5ecb1a468b95f79a56c642ac56
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2021-05-31 16:53:43 +0200

    VIM init xournal launcher

--- vim/init.sh
+++ vim/init.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 
 ln -sr vimrc ~/.vimrc
 ln -sr vimRun.sh ~/bin/vimRun
+ln -sr vim_launcher ~/bin/vim_launcher
 ln -sr bash-rc ~/bin/bash-rc
 mkdir -p ~/.vimRun
 mkdir -p ~/.basicFile
