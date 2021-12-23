1487ad2d5185d74743e00dcd354605ed755a7e6c
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2021-12-23 21:19:27 +0100

    VIM nvim

--- vim/init.sh
+++ vim/init.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 
 confln vimrc ~/.vimrc
+confln nvim.vim ~/.config/nvim/init.vim
 confln vimRun.sh ~/bin/vimRun
 confln vim_launcher ~/bin/vim_launcher
 confln bash-rc ~/bin/bash-rc
