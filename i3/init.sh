07f614b66cfb559ace0ca813cfa1ebe15bbd105f
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2022-11-12 23:26:05 +0100

    I3 icsstatus config

--- i3/init.sh
+++ i3/init.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-confln master.tex ~/.config/i3/
-confln ~/.config/i3/i3.config ~/.config/i3/config
-(cd ~/.config/i3/; tex keymap-i3)
 confln toggle-border ~/.config/i3/i3-toggle-border
 confln kill ~/.config/i3/i3-kill
-confln i3-restart ~/.config/i3/
 confln i3status.conf ~/.config/i3/
+confln i3csstatus.conf ~/.config/i3/
 chmod o+x ~/.config/i3/i3-*
 cat >~/.xinitrc <<EOF
 exec i3
