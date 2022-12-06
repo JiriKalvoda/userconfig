8febadbfa94ec4180f2fdf265e50b9e9cda7f73c
Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2022-12-06 22:55:33 +0100

    I3 Cleanup

--- i3/init.sh
+++ i3/init.sh
@@ -5,9 +5,9 @@ confln toggle-border ~/.config/i3/i3-toggle-border
 confln kill ~/.config/i3/i3-kill
 confln i3status.conf ~/.config/i3/
 confln i3csstatus.conf ~/.config/i3/
-chmod o+x ~/.config/i3/i3-*
 cat >~/.xinitrc <<EOF
 exec i3
 EOF
 g++ status.cpp -o ~/.config/i3/status.out
 
+./config-gen/init.sh
