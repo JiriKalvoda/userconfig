1568a0890fffe94784446d745d4c3166b98af2e0
Arch Root <jirkalvoda@kam.mff.cuni.cz>
2023-05-02 16:05:20 +0200

    USERCONFIG_LIB Big update

--- userconfig-lib.sh
+++ userconfig-lib.sh
@@ -1,6 +1,17 @@
 cd "$(dirname "$0")"
 
 USERCONFIG_ROOT="$1"
+if [[ "$USERCONFIG_ROOT" == "" ]]
+then
+	USERCONFIG_ROOT=$(git rev-parse --show-toplevel)
+	[[ $? != 0 ]] && exit 1
+fi
+
+if ! grep jdskjcnjknksncfdvvndfiuvifd $USERCONFIG_ROOT/userconfig-lib.sh >/dev/null
+then
+	echo "$USERCONFIG_ROOT don't look like userconfig git root."
+	exit 1
+fi
 
 confln()
 {
@@ -22,8 +33,61 @@ git_clupdate()
 	fi
 }
 
+r_exit_on_err=true
+r_pipe_output=true
+r_confirm=false
+
 r()
 {
-	echo "Runnign" 
-	print ""$@"
+	# Run commant with nice log
+	#
+	exit_on_err=$r_exit_on_err
+	do_eval=false
+	while [[ "$1" == "-"* ]]
+	do
+		if [[ "$1" == "-b" ]]
+		then
+			do_eval=true
+		elif [[ "$1" == "-" ]]
+		then
+			break
+		fi
+		shift
+	done
+
+	echo -n ">" 
+	if $do_eval
+	then
+		echo "$@"
+	else
+		printf " %q" "$@"
+		echo
+	fi
+	(
+		if $do_eval
+		then
+			eval "$@"
+		else
+			"$@"
+		fi
+	) 2>&1 | (
+		while read l
+		do
+			echo " │ $l"
+		done
+	)
+	ret="${PIPESTATUS[0]}"
+	if [[ "$ret" == 0 ]]
+	then
+		echo -e " └─   \e[32mOK\e[0m"
+	else
+		if $exit_on_err
+		then
+			echo -e " └─   \e[31mRETURN $ret => EXIT\e[0m"
+			exit $r
+		else
+			echo -e " └─   \e[31mRETURN $ret\e[0m"
+		fi
+	fi
+	return $ret
 }
