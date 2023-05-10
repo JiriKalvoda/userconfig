b63388c808ad8daa01e5d58a2ee9374d75406937
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-05-10 22:31:32 +0200

    userconfig-lib: r

--- userconfig-lib.sh
+++ userconfig-lib.sh
@@ -1,5 +1,12 @@
 cd "$(dirname "$0")"
 
+Red="\e[31m"
+Green="\e[32m"
+Yellow="\e[33m"
+Blue="\e[34m"
+Magenta="\e[35m"
+None="\e[0m"
+
 USERCONFIG_ROOT="$1"
 if [[ "$USERCONFIG_ROOT" == "" ]]
 then
@@ -42,13 +49,20 @@ r()
 	# Run commant with nice log
 	#
 	exit_on_err=$r_exit_on_err
+	pipe_output=$r_pipe_output
+	confirm=$r_confirm
 	do_eval=false
 	while [[ "$1" == "-"* ]]
 	do
-		if [[ "$1" == "-b" ]]
-		then
-			do_eval=true
-		elif [[ "$1" == "-" ]]
+		if [[ "$1" == *b* ]]; then do_eval=true; fi
+		if [[ "$1" == *B* ]]; then do_eval=false; fi
+		if [[ "$1" == *c* ]]; then confirm=true; fi
+		if [[ "$1" == *C* ]]; then confirm=false; fi
+		if [[ "$1" == *e* ]]; then exit_on_err=true; fi
+		if [[ "$1" == *E* ]]; then exit_on_err=false; fi
+		if [[ "$1" == *p* ]]; then pipe_output=true; fi
+		if [[ "$1" == *P* ]]; then pipe_output=false; fi
+		if [[ "$1" == "-" ]] || [[ "$1" == "--" ]]
 		then
 			break
 		fi
@@ -63,20 +77,45 @@ r()
 		printf " %q" "$@"
 		echo
 	fi
-	(
+
+	if $confirm
+	then
+		in=""
+		while [[ "$in" != y ]]
+		do
+			echo -n -e " ╞ $Yellow Run?(Yes/Skip/Exit):$None "
+			read in
+			[[ "$in" == "e" ]] && echo -e " └─  $Magenta Without run (exit) \e[0m" && exit 1
+			[[ "$in" == "s" ]] && echo -e " └─  $Magenta Without run (skip) \e[0m" && return 0
+		done
+	fi
+
+	if $pipe_output
+	then
+		(
+			if $do_eval
+			then
+				eval "$@"
+			else
+				"$@"
+			fi
+		) 2>&1 | (
+			while read l
+			do
+				echo " │ $l"
+			done
+		)
+		ret="${PIPESTATUS[0]}"
+	else
 		if $do_eval
 		then
 			eval "$@"
+			ret="$?"
 		else
 			"$@"
+			ret="$?"
 		fi
-	) 2>&1 | (
-		while read l
-		do
-			echo " │ $l"
-		done
-	)
-	ret="${PIPESTATUS[0]}"
+	fi
 	if [[ "$ret" == 0 ]]
 	then
 		echo -e " └─   \e[32mOK\e[0m"
