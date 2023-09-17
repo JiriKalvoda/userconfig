03b1234440aea291b8f2084b5d051db6fc17c652
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-09-17 13:17:40 +0200

    LIGHT device name changed

--- light/config-default.h
+++ light/config-default.h
@@ -1,7 +1,8 @@
-const int adrlen = 5;
+const int adrlen = 6;
 const char adr [adrlen][1000] =
 {
 	"/sys/class/backlight/amdgpu_bl0/brightness",
+	"/sys/class/backlight/amdgpu_bl1/brightness",
 	"/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness",
 	"/sys/devices/pci0000:00/0000:00:02.0/drm/card1/card1-eDP-1/intel_backlight/brightness",
 	"/sys/devices/pci0000:00/0000:00:08.1/0000:03:00.0/backlight/amdgpu_bl0/brightness",
