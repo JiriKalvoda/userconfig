c27fad5cfe09f162fefa79d403bf82aa5a36cbc3
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-10-11 15:30:46 +0200

    KEYBOARD init: Do not use sudo

--- keyboard/init.sh
+++ keyboard/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-sudo cp /usr/share/X11/xkb/symbols/cz /usr/share/X11/xkb/symbols/cz_backup`date +%s`
-sudo cp cz /usr/share/X11/xkb/symbols/cz
+cp /usr/share/X11/xkb/symbols/cz /usr/share/X11/xkb/symbols/cz_backup`date +%s`
+cp cz /usr/share/X11/xkb/symbols/cz
+
+cp keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
 
