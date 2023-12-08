385c2a566761eb995eb6b7004be34d67149259b1
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-08 15:36:14 +0100

    SCRIPTS booklet fixup

--- scripts/booklet.sh
+++ scripts/booklet.sh
@@ -1,7 +1,7 @@
 pages=`qpdf --show-npages  "$1"`;
 if [ $pages -eq 2 ]; then
 		echo -e " ** [PsPdfTool]\tBuilding booklet (from $pages pages) as landscape split document"
-	paperjam 'spaper(a4r,297mm,210mm) select{1 2 1 2} nup(2,1,paper=a4r)' "$1" "$2"
+	paperjam 'nup(2,paper=a4) rotate(90)' "$1" "$2"
 else
 		echo -e " ** [PsPdfTool]\tBuilding booklet (from $pages pages) as booklet"
 	paperjam 'nup(1,paper=a5) book nup(2,paper=a4)' "$1" "$2"
--- scripts/lpbooklet.sh
+++ scripts/lpbooklet.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 tmp=$(mktemp --suffix=pdf)
-pages=`qpdf --show-npages  "$1"`;
+pages=`qpdf --show-npages  "$1" || true`;
 if [ $pages -eq 2 ]; then
 		echo -e " ** [PsPdfTool]\tBuilding booklet (from $pages pages) as landscape split document"
-	paperjam 'spaper(a4r,297mm,210mm) select{1 2 1 2} nup(2,1,paper=a4r)' "$1" "$tmp"
+	paperjam 'select{1, 2: rotate(180), 1: rotate(180), 2} rotate(90) nup(2,1, paper=a4) rotate(-90)' "$1" "$tmp"
 	lp -o Duplex=DuplexTumble -o XROutputMode=HighResolution "$tmp" "${@:2}"
 else
 		echo -e " ** [PsPdfTool]\tBuilding booklet (from $pages pages) as booklet"
