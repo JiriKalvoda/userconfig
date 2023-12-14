9b0757bbdda3e5b33c26352a7364bf3f4678551e
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-14 11:29:23 +0100

    VOLUME init using lib

--- volume/init.sh
+++ volume/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+. ../userconfig-lib.sh
+need_root
+install_begin
 
-echo -e "\e[31mROOT REQUIRE\e[0m"
+r g++ ./volumeInfo.c -o /usr/bin/volumeInfo
+r chmod u+s /usr/bin/volumeInfo
+r cp ./volumeGUI.sh  /usr/bin/volumeGUI
+r chmod o+x /usr/bin/volumeGUI
 
-g++ ./volumeInfo.c -o /usr/bin/volumeInfo
-chmod u+s /usr/bin/volumeInfo
-cp ./volumeGUI.sh  /usr/bin/volumeGUI
-chmod o+x /usr/bin/volumeGUI
-
-
+install_ok
