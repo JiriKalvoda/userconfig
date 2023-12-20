3592651f03422aa87b19c5444211ffd65bdf4e77
Jiri Kalvoda <jirikalvoda@ucw.cz>
2023-12-20 13:00:19 +0100

    INSPECT-STATE server

--- instpect-state.py
+++ instpect-state.py
@@ -2,26 +2,33 @@
 import sys, os, pathlib
 import subprocess
 import functools
+import argparse
+import tabulate
 
 @functools.cache
 def name_to_path(name, prefix_path=pathlib.Path(".")):
     if name == "":
         if prefix_path.is_dir():
-            return prefix_path/"init.sh"
-        return prefix_path
+            if (prefix_path/"init.sh").is_file():
+                return prefix_path/"init.sh"
+        if prefix_path.is_file():
+            return prefix_path
+        return None
     name = name.split("-")
     variants = []
     for i in range(1, len(name)+1):
         p = prefix_path/("-".join(name[:i]))
-        if p.exists():
+        if p.is_dir() or (p.is_file() and i == len(name)):
             variants.append(("-".join(name[i:]), p))
     # print(name, prefix_path, variants)
-    assert len(variants) == 1
+    if len(variants) == 0:
+        return None
+    assert len(variants) == 1, (name, prefix_path, variants)
     return name_to_path(*variants[0])
 
 @functools.cache
 def current_version(name):
-    name_to_path(name)
+    if name_to_path(name) is None: return None
     p = subprocess.run([name_to_path(name), "-v"], encoding="utf-8", check=True, stdout=subprocess.PIPE)
     return p.stdout.strip()
 
@@ -83,9 +90,36 @@ def load_state_dir(state_dir=pathlib.Path("state")):
             out[name] = Installation(state_dir, name)
     return out
 
-local = load_state_dir()
-table = [[name, local[name].short()] for name in local]
+parser = argparse.ArgumentParser()
+parser.add_argument('--server', '-s', action='store_true')
+
+args = parser.parse_args()
 
 
-from tabulate import tabulate
-print(tabulate(table))
+if args.server:
+    devices = []
+    p = pathlib.Path("../userconfig_state")
+    for i in p.iterdir():
+        device = i.name
+        #print(device)
+        devices.append((device, load_state_dir(i/"state")))
+    devices.sort(key=lambda x: list(reversed(x[0].split("@"))))
+    all_names = set()
+    for name, i in devices:
+        for j in i:
+            all_names.add(j)
+    all_names = list(all_names)
+    all_names.sort()
+    table = [[name, current_version(name)] + [i[name].short() if name in i else "." for  dev_name, i in devices] for name in all_names]
+    head_mach = ["", "v"]
+    head_user = ["", "" ]
+    for name, i in devices:
+        u, m = name.split("@")
+        head_mach.append(m)
+        head_user.append(u)
+    head = [head_mach, head_user, tabulate.SEPARATING_LINE]
+    print(tabulate.tabulate(head+table))
+else:
+    local = load_state_dir()
+    table = [[name, local[name].short()] for name in local]
+    print(tabulate.tabulate(table))
