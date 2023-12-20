ae95572c6bc68a91c3e66c9ab97faec6ee9bb3c9
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-20 16:17:21 +0100

    SCRIPTS init using install

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 . ../userconfig-lib.sh
+install_begin
+
 confln mw2md.sh ~/bin/mw2md
 confln tvim.sh ~/bin/tvim
 confln booklet.sh ~/bin/booklet
 confln lpbooklet.sh ~/bin/lpbooklet
 confln pub ~/bin/
 confln vzt.sh ~/bin/vzt
+
+install_ok
