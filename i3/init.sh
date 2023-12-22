7656eb15f7c5b1641081e894b55c1c106c78fa55
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-22 23:11:47 +0100

    I3 i3cssstatus config could be different on each device (copy, not link)

--- i3/init.sh
+++ i3/init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 . ../userconfig-lib.sh
+version 1
 install_begin
 
 confln toggle-border ~/.config/i3/i3-toggle-border
 confln kill ~/.config/i3/i3-kill
 confln i3status.conf ~/.config/i3/
-confln i3csstatus.conf ~/.config/i3/
+confln i3csstatus.conf ~/.config/i3/ c
 cat >~/.xinitrc <<EOF
 exec i3
 EOF
