ef629b840799c9b9461a2748cdc8045fea16ef86
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-22 22:33:28 +0100

    MYIP only up interfaces

--- myip/myip.sh
+++ myip/myip.sh
@@ -6,4 +6,4 @@ grepCMD="noprefixroute wlp"
 awkCMD='{print $3}'
 . configIp
 
-ip -br addres | grep "$grepCMD"| awk "$awkCMD" | cut -d "/" -f 1 | head -n1
+ip -br addres | grep UP | grep "$grepCMD"| awk "$awkCMD" | cut -d "/" -f 1 | head -n1
