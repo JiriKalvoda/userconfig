db2ad2a5e357d5064cf7063db7b50506b864c420
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-31 21:02:18 +0100

    XOURNAL init using install

--- xournalpp/init.sh
+++ xournalpp/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-. ../userconfig-lib ..
-
+cd "$(dirname "$0")"
+. ../userconfig-lib.sh
+install_begin
 
 confln x ~/bin/
+confln plugins/jk ~/.config/xournalpp/plugins/
 
+install_ok
--- xournalpp/plugins/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. ../../userconfig-lib ../..
-
-
-#confln jk ~/.config/xournalpp/plugins/
-confln jk /usr/share/xournalpp/plugins/
-
