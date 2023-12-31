5d0f243e89a49aeb3feecfdcf8293611bfdca5bf
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-12-31 21:01:53 +0100

    WACOM init using install

--- wacom/init.sh
+++ wacom/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+. ../userconfig-lib.sh
+install_begin
 
 confln wacom-config ~/bin/
-gcc wacom-daemon2.c -o ~/bin/wacom-daemon -lxdo
+r gcc wacom-daemon2.c -o ~/bin/wacom-daemon -lxdo
+
+install_ok
