19f09a37c6024dbef492eef496ff6821dbcc7283
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2024-01-28 19:17:06 +0100

    TEATIMER init using install

--- teatimer/init.sh
+++ teatimer/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+. ../userconfig-lib.sh
+install_begin
 
 ../git-clupdate git://git.ucw.cz/teatimer.git build_git
-(cd build_git ; make)
+r -b "cd build_git ; make"
 confln build_git/teatimer ~/bin/
 confln tt ~/bin/
+
+install_ok
