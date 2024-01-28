b410a04e4ce11721319f0b55b63099d413effcc2
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2024-01-28 21:12:45 +0100

    I3 KEYMAP sway vncserver variant (in vm config)

--- i3/config-gen/init-vm.sh
+++ i3/config-gen/init-vm.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 . ../../userconfig-lib.sh
+version 2
 install_begin
 
 confln main.km ~/.config/i3/config-gen
+confln xrandr-cmddef.km ~/.config/i3/
 
 (
 	cd ~/.config/i3
-	./config-gen > config
+	r mkdir -p ../sway
+	r -b './config-gen > config'
+	r -b './config-gen sway > ../sway/config'
+	r -b './config-gen sway vncserver > ../sway/vnc.config'
 )
 
 install_ok
