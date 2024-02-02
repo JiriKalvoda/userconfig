8cd7ac7faf79e2992fd16b7f385bbced15f882d5
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2024-02-02 23:33:40 +0100

    ALACRITTY yml -> toml

--- alacritty/init.sh
+++ alacritty/init.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 . ../userconfig-lib.sh
 install_begin
 
-confln alacritty.yml ~/.config/alacritty/
+confln alacritty.toml ~/.config/alacritty/
 confln  spawn-alacritty-cwd ~/.config/alacritty/
 
 install_ok
