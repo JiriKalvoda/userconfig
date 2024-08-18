ae90cd5b86291450ae040f9105e61eee20a3896d
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2024-08-18 18:01:54 +0200

    Scripts sfs

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 . ../userconfig-lib.sh
+version 1
 install_begin
 
 confln mw2md.sh ~/bin/mw2md
@@ -9,5 +10,7 @@ confln booklet.sh ~/bin/booklet
 confln lpbooklet.sh ~/bin/lpbooklet
 confln pub ~/bin/
 confln vzt.sh ~/bin/vzt
+
+confln sfs.py ~/bin/ E
 
 install_ok
---
+++ scripts/sfs.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+import subprocess
+import argparse
+import os
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("target")
+parser.add_argument("-u", "--umount", action='store_true')
+args = parser.parse_args()
+
+
+param = args.target
+dirname = param.split('--')[0]
+mountpoint = os.environ['HOME']+"/m/"+dirname
+
+if args.umount:
+    if Path(mountpoint).exists():
+        subprocess.run(["umount", mountpoint], check=True)
+        os.rmdir(mountpoint)
+    for x in os.listdir(os.environ['HOME']+"/m/"):
+        if x.startswith(dirname):
+            os.remove(os.environ['HOME']+"/m/"+x)
+else:
+    try:
+        os.rmdir(mountpoint)
+    except FileNotFoundError:
+        pass
+
+    os.mkdir(mountpoint)
+    follow_symlinks_args = ["-o", "follow_symlinks"]
+    subprocess.run(["sshfs", *follow_symlinks_args, "-C", param+":/", mountpoint], check=True)
+
+    remote_home = subprocess.run(["ssh", param, "pwd"], stdout=subprocess.PIPE).stdout.decode().strip()
+    subprocess.run(["ln", "-sr", mountpoint+remote_home, mountpoint+"~"])
