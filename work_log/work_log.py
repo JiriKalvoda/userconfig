62b655512ac773edf4fb94a967bb4fd195aac964
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2024-11-16 00:20:39 +0100

    WORKLOG stats

--- work_log/work_log.py
+++ work_log/work_log.py
@@ -3,6 +3,8 @@ import argparse
 import datetime
 import sys, os
 import time
+from datetime import datetime, timedelta, date
+import datetime as dt
 
 filename = os.environ['HOME']+'/work_log'
 
@@ -47,23 +49,47 @@ if args.subparser == 'i3status':
         except Exception as e:
             import traceback
             traceback.print_exc()
-        time.sleep(3)
+        time.sleep(1)
 
 if args.subparser == 'log':
     print(args.job)
     with open(filename, 'a') as f:
-        f.write(f"{datetime.datetime.now().astimezone().isoformat()}\t{args.job}\t\n")
+        f.write(f"{datetime.now().astimezone().isoformat()}\t{args.job}\t\n")
 
 if args.subparser == 'stats':
+    by_days = {}
+
+    def divide_per_days(activity, topic, from_time, to_time):
+        for d in [from_time.date() + timedelta(days=x) for x in range(10)]:
+            lim_f = datetime.combine(d, dt.time(0)).astimezone()
+            lim_t = datetime.combine(d, dt.time(0)).astimezone()+timedelta(hours=24)
+            f = max(min(from_time, lim_t), lim_f)
+            t = max(min(to_time, lim_t), lim_f)
+            if t-f:
+                by_days.setdefault(d, {}).setdefault(activity, {}).setdefault(topic, timedelta())
+                by_days[d][activity][topic] += t-f
+
     with open(filename, 'r') as f:
         data = [l.split('\t') for l in f.read().split('\n') if l]
         total_time = {}
+        topics = {}
         for x, y in zip(data, data[1:]):
             activity = x[1]
+            if len(x) >= 2 and x[2]:
+                topics[x[1]] = x[2]
             if activity:
-                t = datetime.datetime.fromisoformat(y[0]) - datetime.datetime.fromisoformat(x[0])
-                total_time.setdefault(activity, datetime.timedelta())
+                divide_per_days(activity, topics.get(activity, "__default__"), datetime.fromisoformat(x[0]), datetime.fromisoformat(y[0]))
+                t = datetime.fromisoformat(y[0]) - datetime.fromisoformat(x[0])
+                total_time.setdefault(activity, timedelta())
                 total_time[activity] += t
+        for d, x in by_days.items():
+            for activity, y in x.items():
+                print(d, activity)
+                for topic, time in y.items():
+                    print(topic, time)
+                print()
+
+
         for k, v in total_time.items():
             print(k, v)
 
