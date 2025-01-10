8e56c7015e4cf2b2925c4b7f9e3841054a9c9250
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2025-01-10 23:05:11 +0100

    I3 KEYBOARD New led strips

--- i3/config-gen/init.sh
+++ i3/config-gen/init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 . ../../userconfig-lib.sh
-version 5
+version 6
 install_begin
 
 confln main.km ~/.config/i3/config-gen
 confln led-cmddef.km ~/.config/i3/
 confln led-blatto-1.km ~/.config/i3/
+confln led-blatto-sp.km ~/.config/i3/
 confln ssh-cmddef.km ~/.config/i3/
 confln xrandr-cmddef.km ~/.config/i3/
 
