2a341feee3bbec49b7d0acd5ad3174e8a9d200dd
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2025-03-16 23:21:19 +0100

    NGINX fixups

--- nginx/init.sh
+++ nginx/init.sh
@@ -8,9 +8,10 @@ install_begin
 confln filter_ngix_config.py /etc/nginx/
 confln nginx.conf /etc/nginx/
 confln blattes_client.crt /etc/nginx/
+confln default_host /etc/nginx/
 confln sites.d/default_host /etc/nginx/sites.d/ d
 confln default_host.d/ifconfig /etc/nginx/default_host.d/ d
-confln filter_ngix_config.conf /etc/systemd/system/nginx.service.d/
+confln filter_ngix_config.conf /etc/systemd/system/nginx.service.d/ c
 
 mkdir -p /etc/nginx/sites_checked.d
 mkdir -p /etc/nginx/default_host_checked.d
