62135771180349f3ec0fc1f6e74dc9ff6bc12a59
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2025-04-08 16:29:07 +0200

    VIM out of tree undofiles

--- vim/init.sh
+++ vim/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 . ../userconfig-lib.sh
+version 1
 install_begin
 
 confln vimrc ~/.vimrc
@@ -10,6 +11,7 @@ confln vim_launcher ~/bin/vim_launcher
 confln bash-rc ~/bin/bash-rc
 mkdir -p ~/.vimRun
 mkdir -p ~/.basicFile
+mkdir -p ~/.local/state/vim/undo/
 for i in basicFile*
 do
 	confln $i ~/.basicFile/
