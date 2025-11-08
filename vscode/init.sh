cf6a7fdcb029422677f143be462dbf5e27470f50
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2025-11-08 22:40:58 +0100

    vscode: install extensions

--- vscode/init.sh
+++ vscode/init.sh
@@ -6,4 +6,18 @@ install_begin
 confln keybindings.json ~/.config/VSCodium/User/
 confln settings.json ~/.config/VSCodium/User/
 
+extensions="
+asvetliakov.vscode-neovim
+rooveterinaryinc.roo-cline
+"
+#leanprover.lean4
+#ms-dotnettools.vscode-dotnet-runtime
+#tamasfe.even-better-toml
+#muhammad-sammy.csharp
+
+for ext in $extensions
+do
+	r vscodium --install-extension $ext
+done
+
 install_ok
